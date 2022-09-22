596a2af35f5eec2e10070c050700ea09b95044da
Pola <Athulya.Pola@sony.com>
2022-09-22 12:29:36 +0530

    first commit

---
+++ src/test/java/com/test/com/test/AppTest.java
@@ -0,0 +1,18 @@
+package com.test.com.test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test(priority=1)
+    public void shouldAnswerWithTrue()
+    {
+       // assertTrue( true );
+    }
+}
